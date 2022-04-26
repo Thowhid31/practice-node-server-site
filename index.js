31540dabe217a7e0f5984dab554033dac80887a7
Thowhid31 <thowhidantor16@gmail.com>
2022-04-27 01:46:25 +0600

    the implement of POST

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors')
 
 app.get('/', (req, res)=>{
     res.send('Hey! Look mama I can code with NODE.js')
 });
+
+app.use(cors());
+app.use(express.json())
 
 const users = [
     {id: 1, name: 'Sabana', email: 'sabana@gmail.com', phone: '01700000000'},
@@ -18,7 +22,14 @@ const users = [
 ]
 
 app.get('/users', (req, res)=>{
-    res.send(users)
+    if(req.query.name){
+        const search = req.query.name.toLowerCase();
+        const matched = users.filter(user => user.name.toLowerCase().includes(search))
+        res.send(matched)
+    }
+    else{
+        res.send(users)
+    }
 });
 
 app.get('/user/:id', (req,res)=>{
@@ -26,6 +37,22 @@ app.get('/user/:id', (req,res)=>{
     const id = parseInt(req.params.id);
     const user = users.find( u => u.id === id)
     res.send(user);
+});
+
+app.post('/user', (req, res)=>{
+    console.log('request', req.body);
+    const user = req.body;
+    user.id =  users.length + 1;
+    users.push(user);
+    res.send(user)
+})  
+
+app.get('/fruits', (req, res)=>{
+    res.send(['Mango', 'Apple', 'Oranges'])
+})
+
+app.get('/fruit/mango/fazle', (req, res)=>{
+    res.send('sour sour fazle flavour')
 })
 
 app.listen(port, ()=>{
